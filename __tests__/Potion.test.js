0ee1014f4c749340853067b44d0dbc3bcabe2513
Mohammad Rahman <mbrahman@gmail.com>
2022-02-25 10:40:14 -0600

    added Player functionality and tests

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -10,7 +10,7 @@ test('creates a new health potion object', () => {
 test('creates a random potion object', () => {
     const potion = new Potion()
 
-    expect(potion.name).toBe(expect.any(String))
+    expect(potion.name).toEqual(expect.any(String))
     expect(potion.name.length).toBeGreaterThan(0)
     expect(potion.value).toEqual(expect.any(Number))
 })
