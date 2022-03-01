cbedb97ba216d28500d08f9e377f3c0449d79aa8
Mohammad Rahman <mbrahman@gmail.com>
2022-03-01 09:39:23 -0500

    initialized game module

---
+++ test.js
@@ -0,0 +1,11 @@
+// 4 constructors
+// parent class of Employee
+// name, id and email
+
+// sublclasses of Manager, Engineer and Intern
+// 
+
+// import inquirer
+// set up questions and answers
+
+// html-template
