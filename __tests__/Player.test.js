2fe770b4c4cf954996d887636f416657410507d8
Mohammad Rahman <mbrahman@gmail.com>
2022-03-03 07:45:20 -0500

    added game logic

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,10 +17,10 @@ test('creates a player object', () => {
 test('gets player\'s stats as an objects', () => {
     const player = new Player('Dave')
 
-    expect(player.getStatus()).toHaveProperty('potions')
-    expect(player.getStatus()).toHaveProperty('health')
-    expect(player.getStatus()).toHaveProperty('strength')
-    expect(player.getStatus()).toHaveProperty('agility')
+    expect(player.getStats()).toHaveProperty('potions')
+    expect(player.getStats()).toHaveProperty('health')
+    expect(player.getStats()).toHaveProperty('strength')
+    expect(player.getStats()).toHaveProperty('agility')
 })
 
 test('gets inventory from player or return false', () => {
